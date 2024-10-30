a57a037b503057b3851cc36ef1f2f6a23ce8d378
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-10-29 21:31:04 -0400

    Added icon picker extention

--- init.lua
+++ init.lua
@@ -261,6 +261,24 @@ require('lazy').setup({
   'mg979/vim-visual-multi',
 
   'm4xshen/autoclose.nvim',
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+
+  {
+      "ziontee113/icon-picker.nvim",
+      config = function()
+          require("icon-picker").setup({ disable_legacy_commands = true })
+
+          local opts = { noremap = true, silent = true }
+
+          vim.keymap.set("n", "<Leader>si", "<cmd>IconPickerNormal<cr>", { noremap = true, silent = true, desc = "Search and insert Icons" })
+          vim.keymap.set("n", "<Leader>sy", "<cmd>IconPickerYank<cr>", { noremap = true, silent = true, desc = "Search and yank Icons" }) --> Yank the selected icon into register
+          vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", { noremap = true, silent = true, desc = "Search Icons" })
+      end
+  },
+
 }, {})
 
 -- [[ Setting options ]]
