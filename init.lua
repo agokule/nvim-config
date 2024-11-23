7cad2e236ce73a53405e5a8b44e7be3acd108503
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-11-23 11:47:25 -0500

    Added debugger for C++

--- init.lua
+++ init.lua
@@ -277,6 +277,60 @@ require('lazy').setup({
       end
   },
 
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      -- Creates a beautiful debugger UI
+      'rcarriga/nvim-dap-ui',
+
+      -- Installs the debug adapters for you
+      'williamboman/mason.nvim',
+
+      'nvim-neotest/nvim-nio'
+    },
+    config = function()
+      local dap = require 'dap'
+      local dapui = require 'dapui'
+
+      -- Basic debugging keymaps, feel free to change to your liking!
+      vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+      vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+      vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+      vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+      vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+      vim.keymap.set('n', '<leader>B', function()
+        dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      end, { desc = 'Debug: Set Breakpoint' })
+
+      -- Dap UI setup
+      -- For more information, see |:help nvim-dap-ui|
+      dapui.setup {
+        icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+        controls = {
+          icons = {
+            pause = '',
+            play = '▶',
+            step_into = '',
+            step_over = '',
+            step_out = '',
+            step_back = '',
+            run_last = '▶▶',
+            terminate = '',
+            disconnect = '',
+          },
+        },
+      }
+
+      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+      vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+      dap.set_exception_breakpoints({"raised", "uncaught"})
+      dap.set_log_level('TRACE')
+    end,
+  }
 }, {})
 
 -- [[ Setting options ]]
@@ -702,7 +756,7 @@ require('code_runner').setup({
     },
     cpp = {
       "cd \"$dir\" &&",
-      "g++ -Wall -static -std=c++17 \"$fileName\"",
+      "g++ -Wall -g -static -std=c++17 \"$fileName\"",
       "-o \"$fileNameWithoutExt\" &&",
       "\"$fileNameWithoutExt\""
     },
@@ -724,6 +778,40 @@ vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent =
 vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
 
 require("autoclose").setup()
+
+-- Debugger stuff
+require('dap').adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = "C:\\Users\\Atharv\\Downloads\\ms-vscode.cpptools-1.23.1@win32-x64.vsix\\extension\\debugAdapters\\bin\\OpenDebugAD7.exe",
+  options = {
+    detached = false
+  }
+}
+require("dap").configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = false,
+    setupCommands = {
+      {
+        text = '-enable-pretty-printing',
+        description =  'enable pretty printing',
+        ignoreFailures = false
+      },
+    },
+  },
+}
+
+require('dap.ext.vscode').load_launchjs(nil, {})
+
+require('dap').configurations.c = require('dap').configurations.cpp
+vim.fn.sign_define('DapBreakpoint', { text = '🔴' })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
