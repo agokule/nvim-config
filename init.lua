2c4d67cb9668ee04706de42c17b6ec3db50af6c1
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-12-12 19:31:33 -0500

    Minor change

--- init.lua
+++ init.lua
@@ -36,7 +36,6 @@ require('lazy').setup({
       "nvim-lua/plenary.nvim",         -- required
       "sindrets/diffview.nvim",        -- optional - Diff integration
 
-      -- Only one of these is needed.
       "nvim-telescope/telescope.nvim", -- optional
     },
     config = true
