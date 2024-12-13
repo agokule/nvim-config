2441ff6fc72ec722982f43f224afec147cbad10b
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-12-13 18:31:04 -0500

    Added dropbar extension

--- init.lua
+++ init.lua
@@ -327,6 +327,20 @@ require('lazy').setup({
         lookup_parents = true,     -- Lookup config files in parent directories
       }
     end
+  },
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end
   }
 }, {})
 -- End of installing packages
