de9540b9216e6d9fd8ca49ce9227066536a3d54d
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-12-14 20:54:18 -0500

    Added GUI functions so that drag and drop works properly

--- init.lua
+++ init.lua
@@ -341,7 +341,8 @@ require('lazy').setup({
       vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
       vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
     end
-  }
+  },
+  'equalsraf/neovim-gui-shim'
 }, {})
 -- End of installing packages
 
