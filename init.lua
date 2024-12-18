43981ba34c389b09339f0bd0afeb6d6b619a5c7f
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-12-18 18:25:39 -0500

    Added coding time for wakatime to status bar

--- init.lua
+++ init.lua
@@ -134,14 +134,6 @@ require('lazy').setup({
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
   },
 
   {
@@ -646,6 +638,12 @@ mason_lspconfig.setup_handlers {
     }
   end,
 }
+--Enable (broadcasting) snippet capability for completion
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
@@ -847,6 +845,76 @@ require("neo-tree").setup({
 
 local neogit = require("neogit")
 neogit.setup {}
+
+local uv = require("luv")
+
+
+local current_time = ""
+local function set_interval(interval, callback)
+    local timer = uv.new_timer()
+    local function ontimeout()
+        callback(timer)
+    end
+    uv.timer_start(timer, interval, interval, ontimeout)
+    return timer
+end
+
+local function update_wakatime()
+    local stdin = uv.new_pipe()
+    local stdout = uv.new_pipe()
+    local stderr = uv.new_pipe()
+
+    local handle, pid =
+        uv.spawn(
+        "wakatime-cli",
+        {
+            args = {"--today"},
+            stdio = {stdin, stdout, stderr}
+        },
+        function(code, signal) -- on exit
+            stdin:close()
+            stdout:close()
+            stderr:close()
+        end
+    )
+
+    uv.read_start(
+        stdout,
+        function(err, data)
+            assert(not err, err)
+            if data then
+                current_time = " " .. data:sub(1, #data - 2) .. " "
+            end
+        end
+    )
+end
+
+set_interval(5000, update_wakatime)
+
+local function get_wakatime()
+    return current_time
+end
+
+require("lualine").setup {
+      options = {
+        icons_enabled = true,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {
+          'encoding',
+          'fileformat',
+          'filetype'
+        },
+        lualine_y = {'progress', get_wakatime},
+        lualine_z = {'location'}
+      }
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
