3b7b2742d0977b3daf014fec1df5011830ee5f63
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-12-31 09:21:36 -0500

    Made relative line numbers

--- init.lua
+++ init.lua
@@ -140,6 +140,8 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+vim.o.relativenumber = true
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
