d5e9d9fda07065277017a01c3ad5fc4358b98100
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-01-01 13:01:36 -0500

    Got a temporary fix for the main.shada.tmp.X files exist error

--- init.lua
+++ init.lua
@@ -258,5 +258,30 @@ mason_lspconfig.setup_handlers {
 -- Configure undotree
 vim.keymap.set('n', '<C-Z>', vim.cmd.UndotreeToggle)
 
+-- temporary fix for https://github.com/neovim/neovim/issues/8587
+-- taken from https://github.com/neovim/neovim/issues/8587#issuecomment-2176399196
+vim.api.nvim_create_user_command("ClearShada", function()
+    local shada_path = vim.fn.expand(vim.fn.stdpath('data') .. "/shada")
+    local files = vim.fn.glob(shada_path .. "/*", false, true)
+    local all_success = 0
+    for _, file in ipairs(files) do
+      local file_name = vim.fn.fnamemodify(file, ":t")
+      if file_name == "main.shada" then
+        -- skip your main.shada file
+        goto continue
+      end
+      local success = vim.fn.delete(file)
+      all_success = all_success + success
+      if success ~= 0 then
+        vim.notify("Couldn't delete file '" .. file_name .. "'", vim.log.levels.WARN)
+      end
+      ::continue::
+    end
+    if all_success == 0 then
+      vim.print("Successfully deleted all temporary shada files")
+    end
+  end,
+  { desc = "Clears all the .tmp shada files" }
+)
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
