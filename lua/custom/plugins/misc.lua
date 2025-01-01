07f5c9cd4bf024eb8f86e7bda7a3cfbb9985381c
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-01-01 18:53:37 -0500

    Added Arpeggio for kj/jk to <Esc> key bind

--- lua/custom/plugins/misc.lua
+++ lua/custom/plugins/misc.lua
@@ -8,5 +8,10 @@ return {
     { 'wakatime/vim-wakatime', lazy = false },
     'equalsraf/neovim-gui-shim',
     'RaafatTurki/hex.nvim',
-    'github/copilot.vim'
+    'github/copilot.vim',
+    { 'kana/vim-arpeggio', config = function ()
+        vim.cmd[[
+Arpeggio imap jk <Esc> " Return to normal mode when jk is pressed
+]]
+    end }
 }
