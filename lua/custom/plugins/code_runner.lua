74b05803e65c77cc6fb79f5b7b7204eb65962303
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-01-01 17:20:49 -0500

    Made the code_runner plugin actually work again

--- lua/custom/plugins/code_runner.lua
+++ lua/custom/plugins/code_runner.lua
@@ -1,45 +1,46 @@
 return {
     "CRAG666/code_runner.nvim",
-    config = true,
-    opt = {
-        filetype = {
-            java = {
-                "cd $dir &&",
-                "javac $fileName &&",
-                "java $fileNameWithoutExt"
+    config = function()
+        require("code_runner").setup({
+            filetype = {
+                java = {
+                    "cd $dir &&",
+                    "javac $fileName &&",
+                    "java $fileNameWithoutExt"
+                },
+                python = "cd \"$dir\" && py \"$fileName\"",
+                typescript = {
+                    "tsc $fileName ; ",
+                    "node $fileName",
+                },
+                rust = {
+                    "cd $dir &&",
+                    "rustc $fileName &&",
+                    "$dir/$fileNameWithoutExt"
+                },
+                cpp = {
+                    "cd \"$dir\" &&",
+                    "g++ -Wall -static -std=c++17 \"$fileName\"",
+                    "-o \"$fileNameWithoutExt\" &&",
+                    "\"$fileNameWithoutExt\""
+                },
+                c = {
+                    "cd \"$dir\" &&",
+                    "gcc -Wall -static \"$fileName\"",
+                    "-o \"$fileNameWithoutExt\" &&",
+                    "\"$fileNameWithoutExt\""
+                },
+                dosbatch = { -- *.bat and *.cmd files
+                    "cd \"$dir\" &&",
+                    "cmd /C \"$fileName\""
+                }
             },
-            python = "cd \"$dir\" && py \"$fileName\"",
-            typescript = {
-                "tsc $fileName ; ",
-                "node $fileName",
-            },
-            rust = {
-                "cd $dir &&",
-                "rustc $fileName &&",
-                "$dir/$fileNameWithoutExt"
-            },
-            cpp = {
-                "cd \"$dir\" &&",
-                "g++ -Wall -g -static -std=c++17 \"$fileName\"",
-                "-o \"$fileNameWithoutExt\" &&",
-                "\"$fileNameWithoutExt\""
-            },
-            c = {
-                "cd \"$dir\" &&",
-                "gcc -Wall -static -std=c17 \"$fileName\"",
-                "-o \"$fileNameWithoutExt\" &&",
-                "\"$fileNameWithoutExt\""
-            },
-            dosbatch = { -- *.bat and *.cmd files
-                "cd \"$dir\" &&",
-                "cmd /C \"$fileName\""
-            }
-        },
-    },
+        })
+    end,
     keys = {
         {'<leader>rc', ':RunCode<CR>', noremap = true, silent = false },
         {'<leader>rf', ':RunFile<CR>', noremap = true, silent = false },
-        {'<leader>rft', ':RunFile tab<CR>', noremap = true, silent = false },
+        {'<leader>rt', ':RunFile tab<CR>', noremap = true, silent = false },
         {'<leader>rp', ':RunProject<CR>',  noremap = true, silent = false },
         {'<leader>rd', ':RunClose<CR>', noremap = true, silent = false },
         {'<leader>crf', ':CRFiletype<CR>', noremap = true, silent = false },
