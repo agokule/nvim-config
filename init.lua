2adad8ccefe09816729b3d31fc4901f7ee0d159a
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-01-05 16:42:35 -0500

    Added user command for :W

--- init.lua
+++ init.lua
@@ -148,5 +148,9 @@ vim.api.nvim_create_user_command("ClearShada", function()
   end,
   { desc = "Clears all the .tmp shada files" }
 )
+
+-- I sometimes type :W instead of :w by accident
+vim.api.nvim_create_user_command('W', 'w', {})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
