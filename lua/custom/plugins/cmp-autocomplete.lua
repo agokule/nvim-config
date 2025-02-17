8d9a2b8399fd9d4068bf6de52d71837503cb4834
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-02-17 17:51:07 -0500

    Made LSP completions colorful

--- lua/custom/plugins/cmp-autocomplete.lua
+++ lua/custom/plugins/cmp-autocomplete.lua
@@ -44,7 +44,10 @@ return {
         'saadparwaiz1/cmp_luasnip',
 
         -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp'
+        'hrsh7th/cmp-nvim-lsp',
+
+        -- Makes LSP completions colorful
+        "xzbdmw/colorful-menu.nvim",
     },
     event = "BufRead",
     config = function()
@@ -88,6 +91,17 @@ return {
                             lazydev = "[LazyDev]",
                         })[entry.source.name]
                     vim_item.kind = cmp_menu_icons[vim_item.kind]
+
+
+                    local highlights_info = require("colorful-menu").cmp_highlights(entry)
+                    -- highlight_info is nil means we are missing the ts parser, it's
+                    -- better to fallback to use default `vim_item.abbr`. What this plugin
+                    -- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
+                    if highlights_info ~= nil then
+                        vim_item.abbr_hl_group = highlights_info.highlights
+                        vim_item.abbr = highlights_info.text
+                    end
+
                     return vim_item
                 end
             },
