44d9e650826735c9f6cd4fce383d6652b893def1
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-02-17 17:04:02 -0500

    Minor change

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -18,7 +18,6 @@ return {
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
       opts = {
         -- recommended settings
         default = {
