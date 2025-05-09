f3d3634c4a0ccd3f1a5f0dc91efaf3f1d2ecb3b1
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-05-09 18:37:59 -0400

    Added keymap to switch between C/C++ header and source files

--- init.lua
+++ init.lua
@@ -145,6 +145,20 @@ vim.keymap.set('t', '<esc>', '<C-\\><C-n>', { desc = 'Escape terminal mode' })
 
 vim.keymap.set('i', '<C-BS>', '<C-W>', { desc = 'Delete the previous word in insert mode' })
 
+vim.keymap.set('n', '<leader>cp', function()
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_file_name = vim.fn.fnamemodify(current_file, ':t:r')
+  local current_file_ext = vim.fn.fnamemodify(current_file, ':e')
+
+  if current_file_ext == 'cpp' then
+    vim.cmd('e ' .. current_file_name .. '.h')
+  elseif current_file_ext == 'h' or current_file_ext == 'hpp' then
+    vim.cmd('e ' .. current_file_name .. '.cpp')
+  else
+    vim.print('Unsupported file extension: ' .. current_file_ext)
+  end
+end, { desc = "Switch between C/C++ header and source file" })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
