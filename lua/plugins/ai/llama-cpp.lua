c76695cf64166b9ba513febb2a2134fd7943e4b4
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-06-11 20:31:23 -0400

    Added commands for llama.vim

--- lua/plugins/ai/llama-cpp.lua
+++ lua/plugins/ai/llama-cpp.lua
@@ -3,5 +3,20 @@ return {
     init = function ()
 	vim.api.nvim_set_hl(0, "llama_hl_hint", {fg = "#e57284", ctermfg=209})
 	vim.api.nvim_set_hl(0, "llama_hl_info", {fg = "#00937b", ctermfg=119})
+
+	vim.g.llama_config = {
+	    auto_fim = false,
+	}
+
+	vim.keymap.set('n', '<leader>ucl', ':LlamaToggle<cr>', { desc = 'Toggle Llama.cpp AI Auto Complete' })
+	vim.keymap.set('n', '<leader>ucm', function ()
+	    vim.g.llama_config.auto_fim = not vim.g.llama_config.auto_fim
+	end, { desc = 'Toggle Llama.cpp AI Auto Complete Manual Mode' })
+
+	vim.keymap.set('n', '<leader>ucc', function ()
+	    vim.ui.select({ "qwen-1.5b", "qwen-3b", "qwen-7b" }, { prompt = "Llama.cpp AI Auto Complete" }, function (choice)
+		vim.fn.setreg('+', "llama-server --fim-" .. choice .. "-default")
+	    end)
+	end, { desc = 'Copy Llama.cpp AI Auto Complete cmd to clipboard' })
     end
 }
