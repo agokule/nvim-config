67f348bd537375d7aa56653f3cf5b2fe033acb2b
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-06-26 17:02:52 -0400

    added temporary fix for windsurf.nvim issue 168

--- lua/plugins/ai/codeium.lua
+++ lua/plugins/ai/codeium.lua
@@ -6,15 +6,24 @@ return {
     },
     event = "InsertEnter",
     main = "codeium",
-    opts = {
-        -- Optionally disable cmp source if using virtual text only
-        enable_cmp_source = true,
-        enable_chat = true,
-        virtual_text = {
-            enabled = true,
-            key_bindings = {
-                accept = "<s-tab>"
+    opts = function()
+        -- temporarary fix for Windows newline handling error
+        -- https://github.com/Exafunction/windsurf.nvim/issues/168
+        if vim.fn.has('win32') == 1 then
+            require('codeium.util').get_newline = function ()
+                return "\n"
+            end
+        end
+        return {
+            -- Optionally disable cmp source if using virtual text only
+            enable_cmp_source = true,
+            enable_chat = true,
+            virtual_text = {
+                enabled = true,
+                key_bindings = {
+                    accept = "<s-tab>"
+                }
             }
         }
-    }
+    end
 }
