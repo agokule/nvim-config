ed6fa580b4b771afa287a5249659acbab53f3efc
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-08-15 17:24:35 -0400

    installed lazygit and got it working

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -132,8 +132,8 @@ return {
         { "<leader>gB", function() Snacks.gitbrowse() end,               desc = "Git Browse",                  mode = { "n", "v" } },
         { "<leader>gb", function() Snacks.git.blame_line() end,          desc = "Git Blame Line" },
         { "<leader>gf", function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
-        { "<leader>gg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
-        { "<leader>gl", function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
+        { "<leader>lg", function() Snacks.lazygit() end,                 desc = "Lazygit" },
+        { "<leader>lG", function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
         { "<leader>un", function() Snacks.notifier.hide() end,           desc = "Dismiss All Notifications" },
         { "<c-/>",      function() Snacks.terminal() end,                desc = "Toggle Terminal" },
         { "<c-_>",      function() Snacks.terminal() end,                desc = "which_key_ignore" },
@@ -170,11 +170,9 @@ return {
         { "<leader>sr", function() Snacks.picker.recent() end, desc = "Recent" },
         -- git
         { "<leader>gr", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-        { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
         { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
         { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
         { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
-        { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
         -- Grep
         { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
         { "<leader>sb", function() Snacks.picker.grep() end, desc = "Grep all files" },
