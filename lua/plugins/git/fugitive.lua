a7706bf053e54dc80c938e727288d48f5dc24913
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2025-08-16 21:09:03 -0400

    Added a keymap to stage everything and commit

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -7,5 +7,6 @@ return {
         { '<leader>gu', ':Git restore .<cr>'},
         { '<leader>gP', ':Git push<cr>' },
         { '<leader>gp', ':Git pull<cr>' },
+        { '<leader>g;', ':Git stage .<cr><esc><c-w>k:q<cr>:Git commit<cr>' }
     }
 }
