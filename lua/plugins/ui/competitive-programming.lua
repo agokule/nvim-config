0a6951cf8c7de2402990603a7887c8c1d2cb370d
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2026-01-04 21:19:45 -0500

    made it easier to use the competitest plugin with by adding more keymaps

--- lua/plugins/ui/competitive-programming.lua
+++ lua/plugins/ui/competitive-programming.lua
@@ -1,21 +1,29 @@
 return {
     'xeluxee/competitest.nvim',
     dependencies = 'MunifTanjim/nui.nvim',
-    config = function()
-        require('competitest').setup({
-            testcases_use_single_file = false,
-            compile_command = {
-                c = { exec = "gcc", args = { "-Wall", "-static", "$(FNAME)", "-o", "$(FNOEXT)" } },
-                cpp = { exec = "g++", args = { "-Wall", "-static", "$(FNAME)", "-o", "$(FNOEXT)" } },
-            },
-            testcases_input_file_format = "s5.$(TCNUM).in",
-            testcases_output_file_format = "s5.$(TCNUM).out"
-        })
-    end,
+    opts = {
+        testcases_use_single_file = false,
+        testcases_single_file_format = "$(FNAME).testcases",
+        testcases_input_file_format = "s5.$(TCNUM).in",
+        testcases_output_file_format = "s5.$(TCNUM).out",
+        testcases_directory = './testcases/',
+        runner_ui = {
+            interface = 'split'
+        },
+        split_ui = {
+            position = 'right'
+        },
+    },
     keys = {
         { '<leader>ct',  ':CompetiTest run<CR>', desc = 'Run CompetiTest' },
         { '<leader>cu',  ':CompetiTest show_ui<CR>', desc = 'Show CompetiTest ui' },
+        { '<leader>ca',  ':CompetiTest add_testcase<CR>', desc = 'Add CompetiTest Testcase' },
+        { '<leader>ce',  ':CompetiTest edit_testcase<CR>', desc = 'Edit CompetiTest Testcase' },
+        { '<leader>cd',  ':CompetiTest delete_testcase<CR>', desc = 'Edit CompetiTest Testcase' },
+        { '<leader>cs',  ':CompetiTest convert files_to_singlefile', desc = 'Convert multiple testcase files to single one'},
+        { '<leader>cm',  ':CompetiTest convert singlefile_to_files', desc = 'Convert single testcase files to multiple ones'},
         { '<leader>crp', ':CompetiTest receive problem<CR>', desc = 'Receive problem (CompetiTest)' },
         { '<leader>crc', ':CompetiTest receive contest<CR>', desc = 'Receive contest (CompetiTest)' },
     },
+    cmd = { 'CompetiTest' }
 }
