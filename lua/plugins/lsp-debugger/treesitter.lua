20f3eef574fdf622a05daa9ea8ed3aca17d0cf7a
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2026-04-18 11:11:20 -0400

    fixed treesitter again

--- lua/plugins/lsp-debugger/treesitter.lua
+++ lua/plugins/lsp-debugger/treesitter.lua
@@ -2,18 +2,17 @@ return {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    branch = 'main',
     event = "LazyFile",
     config = function()
         -- [[ Configure Treesitter ]]
         -- See `:help nvim-treesitter`
         -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
         vim.defer_fn(function()
-            require('nvim-treesitter.configs').setup {
-                ensure_installed = {
-                    -- Add languages to be installed here that you want installed for treesitter
-                    'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'html', 'css',
-                    'cpp', 'c'
-                }
+            require('nvim-treesitter').install {
+                -- Add languages to be installed here that you want installed for treesitter
+                'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'html', 'css',
+                'cpp', 'c'
             }
         end, 0)
     end,
