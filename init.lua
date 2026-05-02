34d2dffb2e1cb9d1a45e185647c9e93b923d22cb
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2026-05-02 17:42:29 -0400

    replaced :ClearShada with an autocommand

--- init.lua
+++ init.lua
@@ -304,30 +304,24 @@ vim.api.nvim_create_autocmd("Filetype", {
 })
 
 -- temporary fix for https://github.com/neovim/neovim/issues/8587
--- taken from https://github.com/neovim/neovim/issues/8587#issuecomment-2176399196
-vim.api.nvim_create_user_command("ClearShada", function()
-    local shada_path = vim.fn.expand(vim.fn.stdpath('data') .. "/shada")
-    local files = vim.fn.glob(shada_path .. "/*", false, true)
-    local all_success = 0
-    for _, file in ipairs(files) do
-      local file_name = vim.fn.fnamemodify(file, ":t")
-      if file_name == "main.shada" then
-        -- skip your main.shada file
-        goto continue
+-- taken from https://github.com/neovim/neovim/issues/8587#issuecomment-3557794273
+vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+  group = vim.api.nvim_create_augroup('fuck_shada_temp', { clear = true }),
+  pattern = { '*' },
+  callback = function()
+    local status = 0
+    for _, f in ipairs(vim.fn.globpath(vim.fn.stdpath('data') .. '/shada', '*tmp*', false, true)) do
+      if vim.tbl_isempty(vim.fn.readfile(f)) then
+        status = status + vim.fn.delete(f)
       end
-      local success = vim.fn.delete(file)
-      all_success = all_success + success
-      if success ~= 0 then
-        vim.notify("Couldn't delete file '" .. file_name .. "'", vim.log.levels.WARN)
-      end
-      ::continue::
     end
-    if all_success == 0 then
-      vim.print("Successfully deleted all temporary shada files")
+    if status ~= 0 then
+      vim.notify('Could not delete empty temporary ShaDa files.', vim.log.levels.ERROR)
+      vim.fn.getchar()
     end
   end,
-  { desc = "Clears all the .tmp shada files" }
-)
+  desc = "Delete empty temp ShaDa files"
+})
 
 -- I sometimes type :W instead of :w by accident
 vim.api.nvim_create_user_command('W', 'w', {})
