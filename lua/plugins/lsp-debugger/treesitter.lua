9c09d5a582093d7da23b8ffe563252cc05798b2e
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2026-05-02 17:24:56 -0400

    replaced nvim-treesitter with tree-sitter-manager.nvim

--- lua/plugins/lsp-debugger/treesitter.lua
+++ lua/plugins/lsp-debugger/treesitter.lua
@@ -1,19 +1,13 @@
+---@type LazySpec
 return {
     -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    branch = 'main',
+    'romus204/tree-sitter-manager.nvim',
     event = "LazyFile",
-    config = function()
-        -- [[ Configure Treesitter ]]
-        -- See `:help nvim-treesitter`
-        -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-        vim.defer_fn(function()
-            require('nvim-treesitter').install {
-                -- Add languages to be installed here that you want installed for treesitter
-                'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'html', 'css',
-                'cpp', 'c'
-            }
-        end, 0)
-    end,
+    cmd = 'TSManager',
+    opts = {
+        ensure_installed = {
+            'lua', 'python', 'tsx', 'javascript', 'typescript', 'vimdoc',
+            'vim', 'bash', 'html', 'css', 'cpp', 'c'
+        }
+    },
 }
