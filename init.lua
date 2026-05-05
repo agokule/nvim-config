9780330ff008a030575db4cd0fff4c15805d7128
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2026-05-04 22:03:41 -0400

    made powershell work without remove-alias error

--- init.lua
+++ init.lua
@@ -26,11 +26,16 @@ vim.cmd('set autochdir')
 
 if vim.fn.has('win32') ~= 0 then
   vim.cmd[[
-    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-    let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
-    let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-    let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
-    set shellquote= shellxquote=
+	   set noshelltemp
+	   let &shell = 'pwsh'
+	   let &shellcmdflag .= '$PSStyle.OutputRendering = ''PlainText'';'
+	   " Workaround (may not be needed in future version of pwsh):
+	   let $__SuppressAnsiEscapeSequences = 1
+	   let &shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+	   let &shellcmdflag .= '[Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();'
+	   let &shellcmdflag .= '$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
+	   let &shellpipe  = '> %s 2>&1'
+	   set shellquote= shellxquote=
   ]]
 else
   if vim.fn.executable('zsh') ~= 0 then
